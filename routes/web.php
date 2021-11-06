b0f90d86b5c0d2b95d79be6e952526f490daca4c
HIROKO HYAKKENDANI <hiro_love_avril-6.ghol-17@ezweb.ne.jp>
2021-11-06 07:48:10 +0000

    L9課題3と4

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function (){
     return view('welcome');
+});
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create','Admin\NewsController@add');
+});
+
+Route::get('xxx/xxx','Admin\AAAController@bbb');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
 });
