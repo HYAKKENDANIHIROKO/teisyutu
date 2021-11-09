608b50fa34fdbef37bfed7b3b1311302ba77217b
HIROKO HYAKKENDANI <hiro_love_avril-6.ghol-17@ezweb.ne.jp>
2021-11-09 08:20:39 +0000

    L11課題

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,13 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create','Admin\NewsController@add');
 });
 
-Route::get('XXX','AAAController@bbb');
+// Route::get('XXX','AAAController@bbb');
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create','Admin\ProfileController@add');
     Route::get('profile/edit','Admin\ProfileController@edit');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
