afd506c7e91c1eb7ab15f61f33290578cc687562
HIROKO HYAKKENDANI <hiro_love_avril-6.ghol-17@ezweb.ne.jp>
2021-11-10 02:52:16 +0000

    L12課題

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -14,7 +14,7 @@
         rel="stylesheet" type="text/css">
      
         <link href="{{secure_asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ secure_asset('css/profile.css') }}" rel="stylesheet">
+        <link href="{{secure_asset('css/admin.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
@@ -23,14 +23,35 @@
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name','Laravel1') }}
                     </a>
-                    <button class="navbar-toggler" type="button" date-toggler="collapse" 
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>    
-                    </button>
+                        <button class="navbar-toggler" type="button" date-toggler="collapse" 
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>    
+                        </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto"></ul>
-                        <ul class="navbar-nav ml-auto"></ul>
+                        <ul class="navbar-nav ml-auto">
+                        @guest
+                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                        @else        
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#"
+                                rorle="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}<span class="caret"></span>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">{{ __('Logout') }}
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>    
+                            </li>
+                       @endguest 
+                       </ul>
                     </div>
                 </div>
             </nav> 
